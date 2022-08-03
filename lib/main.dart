926bfbd2b677c6ef9278cc3e4e7b88ba8a955fc9
CaoThang <thangly2k1@gmail.com>
2022-08-03 19:52:24 +0700

    puhblish release v1.1.0

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Winpe Pay',
+        title: 'Winpe Pay App',
         theme: ThemeData(
           scaffoldBackgroundColor: Colors.white,
           colorScheme: const ColorScheme.light(
