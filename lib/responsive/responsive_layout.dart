f8959f24c92ab0f76078baf105023b9eea2e2667
CaoThang <thangly2k1@gmail.com>
2022-08-04 11:29:10 +0700

    #7 - setup folder responsive & mobile screen layout, folder router, folder utils

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatefulWidget {
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+  }) : super(key: key);
+
+  @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return widget.mobileScreenLayout;
+    });
+  }
+}
