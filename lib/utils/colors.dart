64aba95c62f9cb37747fd377f0a09f44f0860d13
CaoThang <thangly2k1@gmail.com>
2022-08-05 08:20:24 +0700

    #10 - setup google fonts and colors for app

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Colors.white;
+const colorScheme =  Color.fromRGBO(255, 153, 0, 1);
+const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const primaryColor = Color(0xff063046);
+const secondaryColor = Colors.grey;
